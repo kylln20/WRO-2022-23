b6e2c2d1235eccfb4e916677ace2b75eda7d2d92
kylln20 <95261716+kylln20@users.noreply.github.com>
2023-08-06 11:45:12 -0400

    Update VERY_IMPORTANT_CODE.ino

--- src/VERY_IMPORTANT_CODE.ino
+++ src/VERY_IMPORTANT_CODE.ino
@@ -6,11 +6,12 @@ char inputs[5];
 int input;
 
 void setup(){
-  Serial.begin(9600);
+  Serial.begin(115200);
   servoMotor.attach(9);
   servoMotor.write(90);
   BLDCMotor.attach(10, 1000, 2000);
   BLDCMotor.writeMicroseconds(1500);
+  delay(5000);
 }
 
 void loop(){
@@ -19,17 +20,16 @@ void loop(){
   if (Serial.available() > 0) {
     // read incoming serial data:
 
-    Serial.readBytesUntil('\n', inputs, 5);
+    Serial.readBytes(inputs, 5);
     inputs[4] = '\0';
-    delay(40);
     input = atoi(inputs);
-    Serial.println(input);
+    //Serial.println(input);
     if(input >= 2025 && input <= 2095){
-      Serial.println("Servo command");
+      //Serial.println("Servo command");
       servoMotor.write(input-2000);
     }
-    if(input <= 1600 && input >= 1300){
-      Serial.println("Motor command");
+    if(input <= 1600 && input >= 1100){
+      //Serial.println("Motor command");
       BLDCMotor.writeMicroseconds(input);
     }
   }
