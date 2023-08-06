71b47f2d9ea3b28597316daa607904f2c64dc956
kylln20 <95261716+kylln20@users.noreply.github.com>
2023-08-05 22:23:44 -0400

    Add files via upload
    unnecessary image processing code removed

--- src/lineDetection.py
+++ src/lineDetection.py
@@ -22,7 +22,7 @@ while True:
         break
 
 
-speed = 1350
+speed = 1500
 angle = 2060
 ser.write((str(speed) + "\n").encode('utf-8'))
 print("speed: ", speed)
@@ -30,13 +30,13 @@ ser.write((str(angle) + "\n").encode('utf-8'))
 print("angle: ", angle)
             
 #roi coordinates
-points = [(180, 160), (0, 290), (799, 290), (620, 160)]
+points = [(180, 160), (0, 320), (799, 320), (620, 160)]
 
 #set up pi camera
 picam2 = Picamera2()
 picam2.preview_configuration.main.size = (800,600)
 picam2.preview_configuration.main.format = "RGB888"
-picam2.preview_configuration.controls.FrameRate = 60
+picam2.preview_configuration.controls.FrameRate = 80
 picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
@@ -45,11 +45,17 @@ picam2.start()
 proportional=0
 error=0
 prevError=0
-target=-2700
+target=0
 diff=0
 kd=0
-kp=-0.0008
+kp=-0.0005
 
+turning = False
+
+prevFrameTime = 0
+newFrameTime = 0
+
+fpsCount = 0
 
 while True:
     im= picam2.capture_array()
@@ -60,30 +66,31 @@ while True:
     #use edge detection on image
     imGray = cv2.cvtColor(im, cv2.COLOR_BGR2GRAY)
     imgBlur = cv2.GaussianBlur(imGray, (13, 13), 0)
-    ret, imgThresh = cv2.threshold(imGray, 50, 255, cv2.THRESH_BINARY)
+    ret, imgThresh = cv2.threshold(imGray, 50, 255, cv2.THRESH_BINARY_INV)
     v = np.median(imgBlur)
     lowThresh = int(max(0, (1.0 - 0.33) * v))
     highThresh = int(min(180, (1.0 + 0.33) * v))
     
-    imgCanny = cv2.Canny(imgThresh, lowThresh, highThresh)
     #cv2.imshow("grayscale + blur", imBlur) #blurred image that edge detection is used on
-    #cv2.imshow("canny",imgCanny) #image with edge detection
     #cv2.imshow("thresh",imgThresh)
     
-    imgRoi = imgBlur
-    imgRoi = cv2.line(imgRoi, points[0], points[1], (255, 255, 255), 4)
-    imgRoi = cv2.line(imgRoi, points[1], points[2], (255, 255, 255), 4)
-    imgRoi = cv2.line(imgRoi, points[2], points[3], (255, 255, 255), 4)
-    imgRoi = cv2.line(imgRoi, points[3], points[0], (255, 255, 255), 4)
-    cv2.imshow("region of interest", imgRoi)
-    
-    
+    imgRoi = cv2.cvtColor(imgThresh, cv2.COLOR_GRAY2RGB)
+    """
+    imgRoi = cv2.line(imgRoi, points[0], points[1], (0, 155, 0), 4)
+    imgRoi = cv2.line(imgRoi, points[1], points[2], (0, 155, 0), 4)
+    imgRoi = cv2.line(imgRoi, points[2], points[3], (0, 155, 0), 4)
+    imgRoi = cv2.line(imgRoi, points[3], points[0], (0, 155, 0), 4)
+    imgRoi = cv2.line(imgRoi, (200, 200), (600, 200), (0, 155, 0), 4)
+    """
+    #cv2.imshow("region of interest", imgRoi)
+
+    """
     #perspective warping
     width = 800
     height = 600
     original = np.float32(points)
     new = np.float32([(0, 0), (0, height-1), (width-1, height-1), (width-1,0)])
-
+ 
     grid = cv2.getPerspectiveTransform(original, new)
     imgPerspective = cv2.warpPerspective(imgThresh, grid, (width, height), cv2.INTER_LINEAR, borderMode = cv2.BORDER_CONSTANT, borderValue = (0,0,0))
     #imgPerspective = cv2.warpPerspective(imgRoi, grid, (width, height), cv2.INTER_LINEAR, borderMode = cv2.BORDER_CONSTANT, borderValue = (0,0,0))
@@ -94,85 +101,140 @@ while True:
     imgInversePerspective = cv2.bitwise_not(imgPerspective)
     
     ####new regions of interest
-    cv2.rectangle(imgPerspectiveRGB, (0, 150), (200, 600), (0, 255, 0), 2) #left
-    cv2.rectangle(imgPerspectiveRGB, (600, 150), (800, 600), (0, 255, 0), 2) #right
-    
+    cv2.rectangle(imgPerspectiveRGB, (0, 200), (200, 600), (0, 255, 0), 2) #left
+    cv2.rectangle(imgPerspectiveRGB, (600, 200), (800, 600), (0, 255, 0), 2) #right
+    """
     #keep car going straight
     ## initial contour detection
     #contours, hierarchy = cv2.findContours(imgInversePerspective, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     #print(len(contours))
     
-    """
-    for i in contours:
-        area = cv2.contourArea(i)
-        if area > 300:
-            x, y, w, h = cv2.boundingRect(i)
-            cv2.rectangle(imgPerspectiveRGB, (x, y), (x+w, y+h), (0, 255, 0), 2)
-    cv2.imshow("colours!", imgPerspectiveRGB)
-    """
+    leftContours, lefthierarchy = cv2.findContours(imgThresh[100:600, 0:200].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    rightContours, righthierarchy = cv2.findContours(imgThresh[100: 600, 600: 800].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     
-    leftContours, lefthierarchy = cv2.findContours(imgInversePerspective[150:600, 0:200].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    rightContours, righthierarchy = cv2.findContours(imgInversePerspective[150: 600, 600: 800].copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    
+    maxLeftArea = 0
     leftArea = 0
     for i in leftContours:
         leftArea = cv2.contourArea(i)
         if leftArea > 300:
             x, y, w, h = cv2.boundingRect(i)
-            cv2.rectangle(imgPerspectiveRGB, (x, y+150), (x+w, y+h+150), (0, 0, 255), 2) #add crop offset 
+            #cv2.rectangle(imgPerspectiveRGB, (x, y+200), (x+w, y+h+200), (0, 0, 255), 2) #add crop offset 
+            cv2.rectangle(imgRoi, (x, y+100), (x+w, y+h+100), (0, 0, 255), 2)
+            if leftArea > maxLeftArea:
+                maxLeftArea = leftArea
+    leftArea = maxLeftArea
     
+    
+    maxRightArea = 0
     rightArea = 0
     for i in rightContours:
         rightArea = cv2.contourArea(i)
         if rightArea > 300:
             x, y, w, h = cv2.boundingRect(i)
-            cv2.rectangle(imgPerspectiveRGB, (x+600, y+150), (x+w+600, y+h+150), (255, 0, 0), 2) #add crop offset 
+            #cv2.rectangle(imgPerspectiveRGB, (x+600, y+200), (x+w+600, y+h+200), (255, 0, 0), 2)#add crop offset 
+            cv2.rectangle(imgRoi, (x+600, y+100), (x+w+600, y+h+100), (255, 0, 0), 2) 
+            if rightArea > maxRightArea:
+                maxRightArea = rightArea
+    rightArea = maxRightArea
     
     print("left area:", leftArea)
     print("right area:", rightArea)
     
-    error = leftArea-rightArea
-    print("error: ", error)
-    proportional=(target - error)*kp
-    diff=error-prevError
-    motorSteering=proportional+(diff*kd)
-    prevError=error
-    #print(motorsTurningPleaseFix)
-    print(motorSteering)
-    if(motorSteering > -30 and motorSteering < 30):
-        angle = 2060 + motorSteering
-        angle = int(angle)
-        ser.write((str(angle)+"\n").encode('utf-8'))
-        print("angle: ", angle)
-    elif(motorSteering > 30):
-        angle = 2090
-        ser.write((str(angle)+"\n").encode('utf-8'))
-        print("angle: ", angle)
-    else:
-        angle = 2025
-        ser.write((str(angle)+"\n").encode('utf-8'))
-        print("angle: ", angle)
-        
-    speed = 1350
-    ser.write((str(speed) + "\n").encode('utf-8'))
-    print("speed: ", speed)
-
-    cv2.imshow("colours!", imgPerspectiveRGB)
     
-    """
-    #check if should turn
-    num = 0
-    for i in range(190, 600):
-        if imgThresh[165][i] == 255:
-            num = num+1
-            
-    if num/540.0 < 0.15:
+    if(not turning):
+        error = leftArea-rightArea
+        print("error: ", error)
+        proportional=(target - error)*kp
+        diff=error-prevError
+        motorSteering=proportional+(diff*kd)
+        prevError=error
+        print(motorSteering)
+        if(motorSteering > -25 and motorSteering < 25):
+            angle = 2060 + motorSteering
+            angle = int(angle)
+            ser.write((str(angle)+"\n").encode('utf-8'))
+            print("angle: ", angle)
+        elif(motorSteering > 25):
+            angle = 2085
+            ser.write((str(angle)+"\n").encode('utf-8'))
+            print("angle: ", angle)
+        else:
+            angle = 2035
+            ser.write((str(angle)+"\n").encode('utf-8'))
+            print("angle: ", angle)
+        
         speed = 1500
         ser.write((str(speed) + "\n").encode('utf-8'))
         print("speed: ", speed)
-        print("now should turn")
-    """
+    
+    cv2.imshow("colours!", imgRoi)
+    
+    
+    #check if should turn
+    #num = 0
+    #for i in range(200, 600):
+    #    if imgThresh[200][i] == 255:
+    #        num = num+1
+    
+    #if num/540.0 < 0.15:
+    #    turning = True
+    #    print("now turning")
+    #    if(leftArea < rightArea):
+    #        angle = 2035
+    #        ser.write((str(angle) + "\n").encode('utf-8'))
+    #        #print("angle: ", angle)
+    #        speed = 1500
+    #        ser.write((str(speed) + "\n").encode('utf-8'))
+    #        #print("speed: ", speed)
+    #    else:
+    #        angle = 2085
+    #        ser.write((str(angle) + "\n").encode('utf-8'))
+    #        #print("angle: ", angle)
+    #        speed = 1500
+    #        ser.write((str(speed) + "\n").encode('utf-8'))
+    #        #print("speed: ", speed)
+    #else:
+    #    turning = False
+    
+    
+    if leftArea < 100:
+        print("turning left")
+        turning = True
+        angle = 2035
+        ser.write((str(angle) + "\n").encode('utf-8'))
+        print("angle: ", angle)
+        speed = 1500
+        ser.write((str(speed) + "\n").encode('utf-8'))
+        print("speed: ", speed)
+    elif rightArea < 100:
+        print("turning right")
+        angle = 2085
+        ser.write((str(angle) + "\n").encode('utf-8'))
+        print("angle: ", angle)
+        speed = 1500
+        ser.write((str(speed) + "\n").encode('utf-8'))
+        print("speed: ", speed)
+    else:
+        turning = False
+        
     print(" ");
+    
+    newFrameTime = time.time()
+    fps = 1.0/(newFrameTime-prevFrameTime)
+    prevFrameTime = newFrameTime
+    fps = int(fps)
+    print("fps: ", fps)
+    if fps == 0:
+        fpsCount = fpsCount + 1
+    if fpsCount == 10:
+        speed = 1500
+        ser.write((str(speed) + "\n").encode('utf-8'))
+        print("speed: ", speed)
+        
+        angle = 2060
+        ser.write((str(angle) + "\n").encode('utf-8'))
+        print("angle: ", angle)
+        break
     
     if cv2.waitKey(1)==ord('q'):#wait until key ‘q’ pressed
         speed = 1500
